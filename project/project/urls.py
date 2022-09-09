41e37306156827a60793450cf8cd610dd6a90354
Ksenia <kacherepanova@edu.hse.ru>
2022-09-09 13:59:08 +0300

    Stripe Session done

--- project/project/urls.py
+++ project/project/urls.py
@@ -8,8 +8,6 @@ from stripeAPI.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('create-payment-intent/<pk>/', StripeIntentView.as_view(), name='create-payment-intent'),
-    #path('webhooks/stripe/', stripe_webhook, name='stripe-webhook'),
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('success/', SuccessView.as_view(), name='success'),
     path('item/<pk>/', ProductLandingPageView.as_view(), name='landing-page'),
